d692814fd33f0fba7b8ab40c31900fa4c1c5ef20
prasantamang <prashantamang059@gmail.com>
2021-06-25 12:28:50 +0545

    new

---
+++ scripts.js
@@ -0,0 +1,18 @@
+//str.length
+var str = "Hello World!";
+var n = str.length;
+console.log(n);
+
+//str.indexof
+var a = "Hello world, welcome to the universe.";
+var b = str.indexOf("welcome");   
+console.log(b);
+
+//str.lastindexof
+var c = "Hello world, welcome to the universe.";
+var d = str.lastIndexOf("welcome");   
+console.log(d);
+
+//str.indexof(par1, par2)
+
+
